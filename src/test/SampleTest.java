43e7519888bb7e5c1ef26208f35da79dfb3b0736
e185760 <e185760@ie.u-ryukyu.ac.jp>
2018-11-13 00:57:35 +0900

    1st commit

---
+++ src/test/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    void add() {
+        Sample sample = new Sample();
+        int result = sample.add(a:2, b:3);
+        assertEquals(expected:5, result);
+    }
+}
